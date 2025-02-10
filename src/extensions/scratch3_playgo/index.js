c641f9b8caa02121f189f2a46fa06d46c2b727a1
Skybots <jmvelandia90@gmail.com>
2025-02-10 14:30:12 -0500

    Update index.js

--- src/extensions/scratch3_playgo/index.js
+++ src/extensions/scratch3_playgo/index.js
@@ -84,7 +84,7 @@ class Scratch3PlayGoBlocks{
 
     constructor (runtime) {
         this.runtime = runtime
-        this._peripheral = new arduino(runtime, playgo.extensionId)
+        this._peripheral = new arduino(runtime, Scratch3PlayGoBlocks.extensionId)
     }
 
     getInfo () {
