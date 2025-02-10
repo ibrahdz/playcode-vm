d005dbc0f7808b08031cdc5442e4790575e9ee6d
ibrahdz <ibraim@tdrobotica.co>
2025-02-10 14:37:53 -0500

    Update index.js

--- src/extensions/scratch3_playgo/index.js
+++ src/extensions/scratch3_playgo/index.js
@@ -89,7 +89,7 @@ class Scratch3PlayGoBlocks{
 
     getInfo () {
         return {
-            id: playgo.extensionId,
+            id: 'playgo',
             name: 'TDRobotica IoT',
             blockIconURI: blockIconURI,
             showStatusButton: true,
