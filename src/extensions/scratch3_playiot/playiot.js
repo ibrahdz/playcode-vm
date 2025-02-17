f9cd224e3c94ae81246e4dcbf980c58ffcafb168
ibrahdz <ibraim@tdrobotica.co>
2025-02-17 09:24:17 -0500

    Create playiot.js

---
+++ src/extensions/scratch3_playiot/playiot.js
@@ -0,0 +1,75 @@
+const Serial = require('../../io/serial.js')
+
+class PlayIoT {
+	constructor (runtime, extensionId) {
+		this._runtime = runtime
+		this._extensionId = extensionId
+		this._serial = null
+		this._runtime.registerPeripheralExtension(extensionId, this)
+		this.reset = this.reset.bind(this)
+		this._onConnect = this._onConnect.bind(this)
+		this._onMessage = this._onMessage.bind(this)
+		this._pollValues = this._pollValues.bind(this)
+    this.message = null
+    this._messageObj = null
+	}
+
+  scan() {
+    if(this._serial) this._serial.disconnectPeripheral()
+    this._serial = new Serial(
+      this._runtime, 
+      this._extensionId,
+			this._onConnect,
+      this._onMessage
+    )
+  }
+
+  connect(id) {
+    if (this._serial) this._serial.connectPeripheral(id)
+  }
+
+	disconnect () {
+		if (this._serial) {
+			this._serial.disconnectPeripheral()
+			// this.reset();
+		}
+	}
+
+	reset () {
+
+	}
+
+	isConnected () {
+		if(this._serial) {
+			return this._serial.connected
+		} else return false
+	}    
+
+  _onConnect () {
+    
+  }
+
+  _pollValues () {
+
+  }
+
+  _onMessage (value) {
+    this.message = value
+    //Vulnerabilidad complicada
+    try {
+      this._messageObj = JSON.parse(value)
+
+      this.value = this._messageObj.value
+      this.btn = this._messageObj.button
+
+      this.temp = this._messageObj.temp
+
+    } catch (e) {
+      this._messageObj = {
+        error: "Error al parsear el mensaje"
+      }  
+    }
+  }
+
+  isBtnPressed = () => this.btn === 1
+}
