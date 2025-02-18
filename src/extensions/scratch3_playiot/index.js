703fa95d7238d405bcfc9fecd61794bc4663d5d7
ibrahdz <ibraim@tdrobotica.co>
2025-02-18 12:12:36 -0500

    Update index.js

--- src/extensions/scratch3_playiot/index.js
+++ src/extensions/scratch3_playiot/index.js
@@ -130,7 +130,7 @@ class Scratch3PlayIoTBlocks{
                 /* Bloques creados por Ibraim, 12 Feb 2025
                 */
                 {
-                    opcode: 'whenBtnPressed',
+                    opcode: 'whenButtonPressed',
                     text: "al presional el botón [BTN]",
                     blockType: BlockType.HAT,
                     arguments: {
@@ -252,14 +252,14 @@ class Scratch3PlayIoTBlocks{
 	rpPotentiometer = () => this._peripheral.potentiometer
     rpJoystickX = () => this._peripheral.joyx
     rpJoystickY = () => this._peripheral.joyy
-    whenButtonPressed = () => this._peripheral.isBtnPressed()
+    //whenButtonPressed = () => this._peripheral.isBtnPressed()
 
     /**
      * Test whether the A or B button is pressed
      * @param {object} args - the block's arguments.
      * @return {boolean} - true if the button is pressed.
      */
-    /*whenButtonPressed (args) {
+    whenButtonPressed (args) {
         if (args.BTN === 'any') {
             return this._peripheral.buttonA | this._peripheral.buttonB;
         } else if (args.BTN === 'A') {
@@ -268,7 +268,7 @@ class Scratch3PlayIoTBlocks{
             return this._peripheral.buttonB;
         }
         return false;
-    }*/
+    }
 
     /**
      * Test whether the A or B button is pressed
